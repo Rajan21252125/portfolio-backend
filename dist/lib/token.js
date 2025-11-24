1d58290401e55eba233527c806d9597a5aac0a9f
Rajan21252125 <grajan408@gmail.com>
2025-11-24 10:15:20 +0530

    Remove dist from repo

--- dist/lib/token.js
+++
@@ -1,8 +0,0 @@
-// src/lib/tokens.ts
-import * as crypto from "crypto";
-export function generateTokenHex(bytes = 32) {
-    return crypto.randomBytes(bytes).toString("hex"); // plain token for email
-}
-export function hashToken(token) {
-    return crypto.createHash("sha256").update(token).digest("hex");
-}
--- dist/lib/upload.js
+++
@@ -1,9 +0,0 @@
-import cloudinary from "./cloudinary.ts";
-export const uploadBuffer = (buf, options) => new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(options, (err, result) => {
-        if (err)
-            return reject(err);
-        resolve(result);
-    });
-    stream.end(buf);
-});
