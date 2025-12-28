68655673fc775b2cbddc69ea3f63d0e6d54a0f60
Rajan21252125 <grajan408@gmail.com>
2025-12-28 12:41:50 +0530

    added ts dependency

---
+++ index.ts
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import type { Request, Response, NextFunction } from "express";
+
+
+// import your routes
+import profileRoutes from "./routes/profile.routes";
+import projectRoutes from "./routes/projects.routes";
+import authRoutes from "./routes/auth.routes";
+import { requestLogger } from "./middleware/requestLogger";
+import { errorLogger } from "./middleware/errorLogger";
+
+
+const app = express();
+
+// ===== MIDDLEWARE =====
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
+app.use(express.json({ limit: "1mb" }));
+app.use(cookieParser());
+
+// ===== ROUTES =====
+app.get("/", (req, res) => {
+  res.send("Portfolio Backend Running 🚀");
+});
+
+app.use(requestLogger);
+
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+app.use("/projects", projectRoutes);
+
+app.use(errorLogger);
+
+// ===== GLOBAL ERROR HANDLER =====
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error("GLOBAL ERROR:", err);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+
+const isVercel = process.env.VERCEL === "1";
+
+if (!isVercel) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Local server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
